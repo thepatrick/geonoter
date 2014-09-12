6e67fcef3ac63d9bcb5ea109b0efbd1811a6cd07
Patrick Quinn-Graham <make-contact@pftqg.com>
2014-09-12 11:16:48 +1000

    Add Foursquare API to credits

---
+++ GeoNoter/Views/Points/PQGMemoEditorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PQGMemoEditorViewController.swift
+//  GeoNoter
+//
+//  Created by Patrick Quinn-Graham on 12/09/2014.
+//  Copyright (c) 2014 Patrick Quinn-Graham. All rights reserved.
+//
+
+import UIKit
+
+class PQGMemoEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
